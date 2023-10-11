1eb4ea34270b018db233db960c17f9cd9b7a991a
jin <yejin.youn@akqa.com>
2023-10-11 13:10:43 +0200

    adform click animation

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Logo from "./Logo";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Logo />
+      </div>
+    </div>
+  );
+}
